8a0234275d1d050edf420e896544100c7f6487c8
atilla kalay <atilla.kalayy@gmail.com>
2021-03-28 00:39:54 +0300

    28.03.2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,13 +21,10 @@ import { RentalComponent } from './components/rental/rental.component';
     BrandComponent,
     ColorComponent,
     CustomerComponent,
-    RentalComponent
+    RentalComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule,HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,20 +1,26 @@
 import { CarResponseModel } from './../../models/carResponseModel';
 import { Component, OnInit } from '@angular/core';
 import { Car } from 'src/app/models/car';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
-  styleUrls: ['./car.component.css']
+  styleUrls: ['./car.component.css'],
 })
 export class CarComponent implements OnInit {
+  cars: Car[] = [];
+  apiUrl = 'https://localhost:44312/api/cars/getall';
 
-  cars:Car[]=[]
-  // carResponseModel:CarResponseModel={};
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
-
+    this.getCars();
   }
 
+  getCars() {
+    this.httpClient.get<CarResponseModel>(this.apiUrl).subscribe((response) => {
+      this.cars = response.data;
+    });
+  }
 }
